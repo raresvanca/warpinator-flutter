4751592153e62e58203fa880c9b436f12b5b443c
Rares Vanca <raresvanca@gmail.com>
2025-03-31 00:37:31 +0300

    Target platform setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:warpinator/apps/warpinator.dart';
 
 void main() {
+ if (kDebugMode) {
+    // Current development UI target platform 
+    // Change accordingly to the work you are doing
+    debugDefaultTargetPlatformOverride = TargetPlatform.android;
+  }
   runApp(const WarpinatorApp());
 }
